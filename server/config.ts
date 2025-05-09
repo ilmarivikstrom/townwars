fb6a2385833979abfe87c7abd29d5dd34c1961bc
Ilmari Vikström <ilmarimikaelvikstrom@gmail.com>
2025-05-09 09:22:48 +0300

    moving settings.json to server dir

--- server/config.ts
+++ server/config.ts
@@ -1,13 +1,16 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const rawConfig = fs.readFileSync(path.resolve(__dirname, '..', 'settings.json'), 'utf-8');
+const rawConfig = fs.readFileSync(
+  path.resolve(__dirname, "settings.json"),
+  "utf-8"
+);
 const settings = JSON.parse(rawConfig);
 
-const dbFile = path.resolve(__dirname, '..', settings.database.file);
+const dbFile = path.resolve(__dirname, "datadir", settings.database.file);
 
 console.log(__dirname);
 
